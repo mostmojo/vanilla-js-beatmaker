964578793b8709091e59540f3e1061fa2c6f7329
Jacob Papageorgiou <jacob.papageorgiou@gmail.com>
2019-04-05 18:04:15 +0100

    sounds links with event listeners and script tag

--- index.js
+++ index.js
@@ -2,4 +2,11 @@
 window.addEventListener("load", () => {
   const sounds = document.querySelectorAll(".sound");
   const pads = document.querySelectorAll(".pads div");
+
+  // Now... the sound! :)
+  pads.forEach((pad, index) => {
+    pad.addEventListener("click", function() {
+      sounds[index].play();
+    });
+  });
 });
