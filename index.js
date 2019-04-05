46d56f017d75586685c4eab9822b1224aa9dcb64
Jacob Papageorgiou <jacob.papageorgiou@gmail.com>
2019-04-05 17:45:45 +0100

    Adding query selectors to grab sound files and pads

---
+++ index.js
@@ -0,0 +1,5 @@
+// Run content upon page load
+window.addEventListener("load", () => {
+  const sounds = document.querySelectorAll(".sound");
+  const pads = document.querySelectorAll(".pads div");
+});
