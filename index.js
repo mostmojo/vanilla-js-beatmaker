421228e564d0c2839080802ec0fb373ba33ec6e5
Jacob Papageorgiou <jacob.papageorgiou@gmail.com>
2019-04-05 18:31:13 +0100

    Add bubbles and sounds

--- index.js
+++ index.js
@@ -2,11 +2,36 @@
 window.addEventListener("load", () => {
   const sounds = document.querySelectorAll(".sound");
   const pads = document.querySelectorAll(".pads div");
+  const visual = document.querySelector(".visual");
+  const colors = [
+    "#60d394",
+    "#d36060",
+    "#c060d3",
+    "#d3d160",
+    "#6860d3",
+    "#60b2d3"
+  ];
 
-  // Now... the sound! :)
+  // Now... let's get to the magic sounds! :)
   pads.forEach((pad, index) => {
     pad.addEventListener("click", function() {
+      sounds[index].currentTime = 0;
       sounds[index].play();
+
+      createBubbles(index);
     });
   });
+
+  // Create a function that makes bubbles pop up!
+  const createBubbles = index => {
+    const bubble = document.createElement("div");
+    visual.appendChild(bubble);
+    bubble.style.backgroundColor = colors[index];
+    bubble.style.animation = "jump 1s ease";
+  };
 });
+
+// Remember:
+// By calling createBubbles(index); on line 21,
+// it will grab index param from pads.forEach((pad, index)...); lined 16
+// send it to index function on line 26 and into line 29
